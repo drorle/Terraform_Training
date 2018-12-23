9e80fbb6e5d5380010898f93b36532fa6f570c96
c5271711 <dror.levin@sap.com>
2018-12-23 17:06:41 +0200

    Working - Now provisioning an instance and running nginx on it

--- Terraform_-_Getting_Started.Pluralsight/02.demo/moduleone.tf
+++ Terraform_-_Getting_Started.Pluralsight/02.demo/moduleone.tf
@@ -2,11 +2,12 @@
 # VARIABLES
 ##################################################################################
 
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+#variable "aws_access_key" {}
+#variable "aws_secret_key" {}
 variable "private_key_path" {}
+variable "public_key" {}
 variable "key_name" {
-  default = "PluralsightKeys"
+  default = "terraform-key-pair"
 }
 
 ##################################################################################
@@ -14,29 +15,36 @@ variable "key_name" {
 ##################################################################################
 
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  region     = "us-east-1"
+#  access_key = "${var.aws_access_key}"
+#  secret_key = "${var.aws_secret_key}"
+#  region     = "us-west-2"
 }
 
 ##################################################################################
 # RESOURCES
 ##################################################################################
+resource "aws_key_pair" "deployer" {
+  key_name   = "${var.key_name}"
+  public_key = "${var.public_key}"
+}
 
 resource "aws_instance" "nginx" {
-  ami           = "ami-c58c1dd3"
+  ami           = "ami-076e276d85f524150"
   instance_type = "t2.micro"
+  security_groups = ["SSH","open ports"]
   key_name        = "${var.key_name}"
 
   connection {
-    user        = "ec2-user"
+    type        = "ssh"
+    agent       = "false"
+    user        = "ubuntu"
     private_key = "${file(var.private_key_path)}"
   }
 
   provisioner "remote-exec" {
     inline = [
-      "sudo yum install nginx -y",
-      "sudo service nginx start"
+      "sudo apt-get update",
+      "sudo apt-get install -y nginx"
     ]
   }
 }
